fa7b32362d23515c15780f5355c76d97dd4ef871
Chelsea Du <chsea.du@gmail.com>
2015-07-26 23:47:56 -0400

    initial commit

---
+++ haiku.js
@@ -0,0 +1,53 @@
+var fs = require('fs');
+
+fs.readFile('cmudict.txt', function(err, data) {
+  if (err) {
+    return console.log(err);
+  }
+
+  var dictionary = [];
+  var lines = data.toString().split('\n');
+  lines.forEach(function(line) {
+    var line_split = line.split('  ');
+    var word = line_split[0];
+
+    var count = line_split[1].split(' ').reduce(function(count, syl) {
+      if (syl.match(/\d/)) return ++count;
+      else return count;
+    }, 0);
+
+    dictionary.push([word, count]);
+  });
+
+  var sylStructure = [genSylStructure(5), genSylStructure(7), genSylStructure(5)];
+
+  sylStructure.forEach(function(sylPerLine) {
+    var line = [];
+    sylPerLine.forEach(function(syl) {
+      line.push(getWordBySyl(dictionary, syl))
+    });
+
+    console.log(line.join(' '));
+  });
+});
+
+function getWordBySyl(dictionary, syl) {
+  var wordsBySyl = dictionary.filter(function(entry) {
+    return entry[1] == syl;
+  });
+
+  return wordsBySyl[Math.floor(Math.random()*wordsBySyl.length)][0];
+}
+
+function genSylStructure(numSyls) {
+  var syls = [Math.floor(Math.random()*numSyls) + 1];
+  var total = syls[0];
+
+  while (total < numSyls) {
+    var newSyl = Math.floor(Math.random()*(numSyls - total)) + 1;
+    syls.push(newSyl);
+    total += newSyl;
+  }
+
+  return syls;
+}
